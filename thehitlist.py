7c32b59eda830446df9560f9e1806bcef9865797
Matt Diephouse <matt@diephouse.com>
2009-04-10 17:49:51 -0700

    Find tasks by tags and get/set titles

--- thehitlist.py
+++ thehitlist.py
@@ -1,6 +1,22 @@
 
 import appscript
 
-class Task:
-    pass
+class Task(object):
+    thl = appscript.app('The Hit List')
+    
+    @classmethod
+    def find_tagged(cls, tagname):
+        for tag in cls.thl.tags_group.groups.get():
+            if tag.name.get() == tagname:
+                return [Task(t) for t in tag.tasks.get()]
+        return []
+    
+    def __init__(self, osatask):
+        self.osatask = osatask
+    
+    def gettitle(self):
+        return self.osatask.title.get()
+    def settitle(self, value):
+        self.osatask.title.set(value)
+    title = property(gettitle, settitle)
 
