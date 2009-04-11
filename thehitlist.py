9f8611e52a45107546cdc91387a91bd969eb2fd8
Matt Diephouse <matt@diephouse.com>
2009-04-11 00:41:02 -0700

    Add a class for the application, as well as Tag and Group classes

--- thehitlist.py
+++ thehitlist.py
@@ -1,14 +1,40 @@
 
 import appscript
 
-class Task(object):
-    thl = appscript.app('The Hit List')
+
+class Group(object):
+    def __init__(self, osagrp):
+        self.osagrp = osagrp
+
+    def getname(self):
+        return self.osagrp.name.get()
+    def setname(self, value):
+        self.osagrp.name.set(value)
+    name = property(getname, setname)
+    
+    @property
+    def tasks(self):
+        return [Task(t) for t in self.osagrp.tasks.get()]
+
+
+class Tag(Group):
+    pass    
+
+
+class TheHitList(object):
+    app = appscript.app('The Hit List')
     
     @classmethod
+    def tags(cls):
+        return [Tag(t) for t in cls.app.tags_group.groups.get()]
+
+
+class Task(object):
+    @classmethod
     def find_tagged(cls, tagname):
-        for tag in cls.thl.tags_group.groups.get():
-            if tag.name.get() == tagname:
-                return [Task(t) for t in tag.tasks.get()]
+        for tag in TheHitList.tags():
+            if tag.name == tagname:
+                return tag.tasks
         return []
     
     def __init__(self, osatask):
